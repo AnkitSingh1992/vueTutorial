0413020066362400b41b44d212d20c89a8607845
Ankit kumar singh <ankitsngh70@gmail.com>
2022-10-09 11:55:01 +0530

    Compostion Api component

---
+++ vue-compostion/src/components/ReactiveUpdate.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+     <h3>{{firstName}} {{lastName}}</h3>
+  </div>
+</template>
+
+<script>
+import {reactive,toRefs} from 'vue'
+export default {
+    name:'ReactiveUpdate',
+    setup(){
+        const state = reactive({
+            firstName: 'Ankit',
+            lastName:'Singh',
+           
+        });
+        //setupTimeout
+        setTimeout(()=>{
+            console.log("Reactive time out")
+            state.firstName = 'rahul',
+            state.lastName = 'tiwari'
+        },2000)
+
+        return toRefs(state)           
+    }
+
+}
+</script>
+
+<style>
+
+</style>
